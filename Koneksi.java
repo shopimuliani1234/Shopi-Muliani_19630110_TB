93f8131cf63af3a889915189faa611b0396a30da
shopimuliani1234 <97204601+shopimuliani1234@users.noreply.github.com>
2022-01-06 09:12:03 +0700

    Add files via upload

---
+++ Koneksi.java
@@ -0,0 +1,37 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Barangpc;
+
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+/**
+ *
+ * @author ACER
+ */
+public class Koneksi {
+   private static java.sql.Connection koneksi;
+   
+   
+   public static java.sql.Connection getKoneksi(){
+     if (koneksi == null){
+         try {
+           String url = "jdbc:mysql://localhost:3306/sewa_pc_0110";
+           String user ="root";
+           String password ="";
+           DriverManager.registerDriver(new com.mysql.jdbc.Driver());
+           koneksi = DriverManager.getConnection(url, user, password);
+           System.out.println("Berhasil Terhubung");
+         }catch(SQLException e){
+             System.out.println("Error");
+         }
+     }
+     return koneksi;
+   }
+   public static void main(String args[]){
+     getKoneksi();
+   }  
+}
